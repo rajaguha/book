1e7a1fb3b9834eb9e37c25d559f1a1fb76da3c85
Raja Guha-Mustafi <rajag@risk-averse.trade>
2023-06-30 17:00:05 +0530

    add matches; correct launch.json

---
+++ matches/src/matches.rs
@@ -0,0 +1,20 @@
+// use std::env;
+
+#[allow(dead_code)]
+
+fn main() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(5) => Some(10),
+            Some(6) => Some(11),
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    println!( "Five: {:?}", plus_one(five) );
+    println!( "Six: {:?}", plus_one(Some(6)) );
+    println!( "Other: {:?}", plus_one(Some(27)) );
+    println!( "None: {:?}", plus_one(None) );
+}
