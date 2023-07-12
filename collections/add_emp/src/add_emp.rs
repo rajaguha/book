317621f4b8b32f3691919af17e59b2d72fd696d7
Raja Guha-Mustafi <rajag@risk-averse.trade>
2023-07-12 19:54:09 +0530

    completed collections

--- collections/add_emp/src/add_emp.rs
+++ collections/add_emp/src/add_emp.rs
@@ -8,16 +8,16 @@
  *  Add <First Middle Last> to <Department>.
  *  Remove <First Middle Last> from <Department>.
  *  List all in <Department>.
- *  List all by department. | List all
- * Quit
+ *  List all by department. | List all.
+ *  Quit
  */
 
- use std::io;
+use std::io;
 use regex::Regex;
 use std::collections::HashMap;
 
 fn main() {
-    let mut emp_list: HashMap<&str, &mut Vec<&str>> = HashMap::new();
+    let mut emp_list: HashMap<String, Vec<String>> = HashMap::new();
 
     let re_cmd = Regex::new( r"^\b(?<cmd>Add|Remove|List|Quit)\b" ).unwrap();
     loop {
@@ -34,41 +34,71 @@ fn main() {
         let cmd: &str = &caps["cmd"];
 
         match cmd {
-            "Add" => add_empl( &emp_list, stmt ),
+            "Add"  => add_empl( &mut emp_list, stmt ),
             // "Remove" => del_empl( &emp_list, stmt ),
-            // "List" => lst_empl( &emp_list, stmt ),
+            "List" => lst_empl( &emp_list, stmt ),
             "Quit" => return,
-            &_ => {
-                println!( "Command '{cmd}' unknown. Try again." );
-                continue;
-            }   
+            &_ => println!( "Command '{cmd}' unknown. Try again." ),
         } 
     }
 }
 
-fn add_empl( list: &HashMap<&str, &mut Vec<&str>>, stmt: &str ) {
-    let re_add = Regex::new( r"^\w+ (?<name>(?:[A-Z][a-z]+ )+)to (?<dept>[A-Z][a-z]+).$" ).unwrap();
-    let Some( caps ) = re_add.captures(stmt) else {
-        println!( "'{stmt}' not comprehensible. Try again." );
+fn lst_empl( list: &HashMap<String, Vec<String>>, stmt: &str ) {
+    let re_lst = Regex::new( r"^List all(?: (?:in|by) (?<dept>[_0-9A-Za-z]+))?.$" ).unwrap();
+    let Some( caps ) = re_lst.captures(stmt) else {
+        println!( "\t'{stmt}' not comprehensible. Try again." );
         return;
     };
-    let (_, [name, dept]) = caps.extract();
-    println!( "Cmd: Add; Name: {name}; Dept: {dept}" );
+    let dept = caps.name("dept").map_or("department", |m| m.as_str());
 
-    let vec: Option<&&mut Vec<&str>> = list.get(dept);
+    if dept == "department" {
+        for key in list.keys() {
+            let vec = list.get(key).unwrap();
+            println!( "\tIn department {key}:" );
+            for emp in vec {
+                println!( "\t\t{emp}" );
+            }
+        }
+    } else {
+        let vec: Option<&Vec<String>> = list.get(dept);
+        match vec {
+            None => { 
+                println!( "\t'{dept}' - no such department." );
+                return;
+            },
+            Some(vec) => {
+                println!( "\tFor department {dept}:" );
+                for emp in vec {
+                    println!( "\t\t{emp}" );
+                }
+            },
+        }
+    }
+}
+
+fn add_empl( list: &mut HashMap<String, Vec<String>>, stmt: &str ) {
+    let re_add = Regex::new( r"^Add (?<name>(?:[A-Z][a-z]+(?: |-))+)to (?<dept>[A-Z][_0-9a-z]+).$" ).unwrap();
+    let Some( caps ) = re_add.captures(stmt) else {
+        println!( "\t'{stmt}' not comprehensible. Try again." );
+        return;
+    };
+    let (_, [name, dept, ]) = caps.extract();
+    let name = name.trim();
+
+    let vec: Option<&mut Vec<String>> = list.get_mut(dept);
     match vec {
-        None =>{
-            let vec: &mut Vec<&str> = &mut vec![name];
-            list.insert( dept, &mut vec );
-            println!( "{dept} has been added to list." );
-            println!( "{name} has been added to {dept}." );
+        None => {
+            let mut vec = Vec::new();
+            vec.push( name.to_string() );
+            list.insert( dept.to_string(), vec );
+            println!( "\tNew department {dept} with {name} has been added." );
         },
         Some(vec) => {
-            if vec.contains(&name) {
-                println!( "{name} is already in {dept}!" );
+            if vec.contains( &name.to_string() ) {
+                    println!( "\t{name} is already in {dept}!" );
             } else {
-                vec.push(name);
-                println!( "{name} has been added to {dept}." );
+                vec.push( name.to_string() );
+                println!( "\t{name} has been added to {dept}." );
             }
         },
     }
@@ -81,16 +111,6 @@ fn add_empl( list: &HashMap<&str, &mut Vec<&str>>, stmt: &str ) {
 //         return;
 //     };
 //     let (_, [name, dept]) = caps.extract();
-
 //     println!( "Cmd: Add; Name: {name}; Dept: {dept}" );
-// }
-// fn lst_empl( _list: &HashMap<&str, mut Vec<&str>>, stmt: &str ) {
-//     let re_lst = Regex::new( r"\^List all( ?:in|by (?<dept>\w+))?.$" ).unwrap();
-//     let Some( caps ) = re_lst.captures(stmt) else {
-//         println!( "'{stmt}' not comprehensible. Try again." );
-//         return;
-//     };
-//     let (_, [dept]) = caps.extract();
 
-//     println!( "Cmd: Add; Dept: {dept}" );
 // }
--- collections/median_mode/src/median_mode.rs
+++ collections/median_mode/src/median_mode.rs
@@ -1,11 +1,41 @@
-#[allow(dead_code)]
+/******
+ * Given a list of integers, use a vector and return the median (when 
+ * sorted, the value in the middle position) and mode (the value that 
+ * occurs most often; a hash map will be helpful here) of the list.
+ **/
+
+use rand::Rng;
+use std::collections::HashMap;
 
 fn main() {
-    println!("Hello, median_mode!");
+    let mut vec: Vec<u32> = Vec::new();
+    for _i in 1..200 {
+        let rn: u32 = rand::thread_rng().gen_range(1..=100);
+        vec.push(rn);
+    }
+    vec.sort();
 
-    use std::env;
-    let args: Vec<_> = env::args().collect();
-    for (i, arg) in args.iter().enumerate() {
-        println!("\targ{i}: {:#?}", arg);
+    // Median
+    let count = vec.len();
+    let mid: f32 = count as f32 / 2.0;
+    if count % 2 == 0 {
+        let mid_lo: usize = mid.floor() as usize;
+        let mid_hi: usize = mid.ceil() as usize;
+        let median = ( vec[mid_lo] + vec[mid_hi] ) as f32 / 2.0;
+        println!( "The median is {median}." );
+    } else {
+        println!( "The median is {}.", vec[mid.ceil() as usize] );
+    }
+
+    // Mode
+    let mut hm: HashMap<u32, u32> = HashMap::new();
+    let mut it: (u32, u32) = (0, 0);
+    for &val in vec.iter() {
+        let nxt = *(hm.entry(val).or_insert(0)) + 1;
+        hm.insert(val, nxt);
+        if nxt > it.1 {
+            it = (val, nxt);
+        }
    }
+   println!("The mode is {} with {} occurances.", it.0, it.1);
 }
